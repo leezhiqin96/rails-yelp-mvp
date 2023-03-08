2176f242797225b34a86f2a89b7725b62c2692bb
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-08 12:34:40 +0800

    Added validation for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :name, :address, presence: true
+  has_many :reviews, dependent: :destroy
 end
