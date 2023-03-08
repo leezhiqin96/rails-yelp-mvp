7b95a3cd60af9d38887d0f5f4790dbb8bc8c35ea
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-08 16:16:47 +0800

    Added create method for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+  # GET "restaurants/:restaurant_id/reviews/new"
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
-  def create; end
+  # Post "restaurants/:restaurant_id/reviews"
+  def create
+    @review = Review.new(reviews_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:rating, :content, :restaurant_id)
+  end
 end
