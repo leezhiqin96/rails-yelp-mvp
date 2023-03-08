1ebad367b76ff7caf91991c76da983f638dd8f7d
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-08 15:34:36 +0800

    Adds review controllers with new method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :restaurants
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
